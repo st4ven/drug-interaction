082ca82f2e758bf92fceb387a5a1dfdce3bebf00
Steven <57573122+st4ven@users.noreply.github.com>
2024-08-21 19:21:48 -0700

    Install cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const app = express();
 const {checkInteractions} = require('./queries');
+const cors = require('cors');
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+}
+
+app.use(cors(corsOptions));
 
 app.get('/check-interaction', async (req, res) => {
   const {drug1, drug2, drug3, drug4, drug5} = req.query;
