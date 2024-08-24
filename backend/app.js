5cb99b3ae37b6d3a1455e5cd7d16145fbcb49d97
Steven <57573122+st4ven@users.noreply.github.com>
2024-08-24 10:40:31 -0700

    Reverting back to 68fd31c

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+// app.js
+const express = require('express');
+const app = express();
+const {checkInteractions} = require('./queries');
+const cors = require('cors');
+const corsOptions = {
+  origin: ["http://localhost:5173"]
+}
+
+app.use(cors(corsOptions));
+
+app.get('/check-interaction', async (req, res) => {
+  const {drug1, drug2, drug3, drug4, drug5, drug6} = req.query;
+  const drugs = [drug1, drug2, drug3, drug4, drug5, drug6].filter(Boolean);
+
+
+  const interactions = await checkInteractions(drugs);
+
+  res.json({interactions});
+});
+
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
